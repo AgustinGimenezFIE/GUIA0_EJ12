9fb71763f42e9a59b3b9ebdfa171a57e99b58cb6
Agustin <darkstone19@gmail.com>
2023-04-19 21:53:23 -0300

    Primer commit

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+/*
+ EJERCICIO 12 : Implemente una función que reciba un string por referencia y lo invierta. Por ejemplo, si el string es "hola", el resultado debe ser "aloh".
+*/
 int main() {
     printf("Hello, World!\n");
     return 0;
